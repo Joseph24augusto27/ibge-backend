7eab48ae68c3454526d5704746d97176c358619f
joseph24augusto27 <josephinho2000@gmail.com>
2026-02-07 21:42:14 -0300

    alteração no server.js

--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ app.use(express.json())
 app.use("/api/ibge", ibgeRoutes)
 
 
-app.listen(3333, () => {
-  console.log("🚀 Server running on port 3333")
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`)
 })
+
